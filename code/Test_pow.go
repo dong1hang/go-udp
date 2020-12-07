9cd8d2f749adb2e8d7c0e1c142f02782de0320ef
Sven <dong3hang@gmail.com>
2020-12-07 23:47:37 +0900

    update

--- code/Test_pow.go
+++ code/Test_pow.go
@@ -4,16 +4,18 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
-
-	for i := 0; i < 10000; i++ {
+	start := time.Now()                //当前时间
+	for i := 0; i < 10000000000; i++ { //循环挖矿
 		data := sha256.Sum256([]byte(strconv.Itoa(i)))
 		fmt.Printf("%10d,%x\n", i, data)
-		fmt.Printf("%s\n", string(data[len(data)-2:]))
-		if string(data[len(data)-2:]) == "00" {
-			fmt.Println("挖矿成功")
+		fmt.Printf("%s\n", string(data[len(data)-1:]))
+		if string(data[len(data)-3:]) == "000" {
+			usedtime := time.Since(start)
+			fmt.Println("挖矿成功", usedtime)
 			break
 		}
 	}
